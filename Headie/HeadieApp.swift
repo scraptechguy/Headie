156335aff979ed90b02de5172894740314a2bba7
Rostislav Brož <scraptechguy@gmail.com>
2022-12-29 16:28:53 +0100

    Initial Commit

---
+++ Headie/HeadieApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadieApp.swift
+//  Headie
+//
+//  Created by Rostislav Brož on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HeadieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
