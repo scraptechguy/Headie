318373744d5b17be649b7ff97028e83985a974a0
Rostislav Brož <scraptechguy@gmail.com>
2022-12-31 11:41:44 +0100

    Create the Location section in `ContentModel`

--- Headie/ViewModels/ContentModel.swift
+++ Headie/ViewModels/ContentModel.swift
@@ -12,6 +12,72 @@ import MapKit
 
 class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     
-    
+    // MARK: - Location
+      
+      var locationManager = CLLocationManager()
+
+      @Published var authorizationState = CLAuthorizationStatus.notDetermined
+      @Published var placemark: CLPlacemark?
+      
+      override init() {
+          
+          // Init method of NSObject
+          super.init()
+          
+          // Make ContentModel the delegate of the location manager
+          locationManager.delegate = self
+          
+      }
+      
+      // Request permission
+      func requestGeolocationPermission() {
+          
+          locationManager.requestWhenInUseAuthorization()
+          
+      }
+      
+      // MARK - Location Manager Delegate Methods
+      
+      func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+          
+          authorizationState = locationManager.authorizationStatus
+          
+          if locationManager.authorizationStatus == .authorizedAlways || locationManager.authorizationStatus == .authorizedWhenInUse {
+              
+              locationManager.startUpdatingLocation()
+              
+          } else if locationManager.authorizationStatus == .denied {
+              
+              
+              
+          }
+          
+      }
+      
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+          
+          let userLocation = locations.first
+          
+          if userLocation != nil {
+              
+              // Stop updating location after received once
+              locationManager.stopUpdatingLocation()
+              
+              // Get the placemark of the user
+              let geoCoder = CLGeocoder()
+              
+              geoCoder.reverseGeocodeLocation(userLocation!) { (placemarks, error) in
+                  
+                  // Check for errors
+                  if error == nil && placemarks != nil {
+                      
+                      self.placemark = placemarks?.first
+                      
+                  }
+              }
+              
+          }
+          
+      }
     
 }
