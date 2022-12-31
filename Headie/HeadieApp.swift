754789e16e067d2fc473fce469c04522547b4e19
Rostislav Brož <scraptechguy@gmail.com>
2022-12-31 11:43:18 +0100

    Pass the right file to App struct

--- Headie/HeadieApp.swift
+++ Headie/HeadieApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct HeadieApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
+                .environmentObject(ContentModel())
         }
     }
 }
